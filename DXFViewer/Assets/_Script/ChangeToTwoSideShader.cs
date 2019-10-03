3375aa96b74d4ee84a0f5e93fc651b937f1042d1
dada_kuo <kuo.data@nuwarobotics.com>
2019-10-03 19:19:03 +0800

    新增匯出obj的功能，雙面shader轉換進行中

---
+++ DXFViewer/Assets/_Script/ChangeToTwoSideShader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToTwoSideShader : MonoBehaviour {
+
+
+
+	void Start () {
+        Material mat = new Material(Shader.Find("Standard(twoSide)"));
+
+        MeshRenderer[] meshs = GameObject.FindObjectsOfType<MeshRenderer>() as MeshRenderer[];
+        foreach (var item in meshs)
+        {
+            item.material = mat;
+        }
+    }
+
+}
--- DXFViewer/Assets/_Script/GenerateMesh.cs
+++ DXFViewer/Assets/_Script/GenerateMesh.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
 using UnityEngine;
 
 public class GenerateMesh : MonoBehaviour
@@ -21,6 +24,7 @@ public class GenerateMesh : MonoBehaviour
 
     GoLine goLines;
     LineRenderer lineRenderer;
+
     void Start()
     {
         goLines = GetComponent<GoLine>();
@@ -32,12 +36,12 @@ public class GenerateMesh : MonoBehaviour
     {
 
         if (Input.GetKeyDown(KeyCode.G)){
-           
-            Debug.Log(goLines.PositionStart+"=="+ goLines.PositionEnd);
+
+            Debug.Log(goLines.PositionStart + "==" + goLines.PositionEnd);
 
             LineStartPosition = goLines.PositionStart;
             LineEndPosition = goLines.PositionEnd;
-            
+
             Vector3 Vertex1Position = new Vector3(LineStartPosition.x, 0, LineStartPosition.y);
             Vector3 Vertex2Position = new Vector3(LineEndPosition.x, 0, LineEndPosition.y);
             Vector3 Vertex3Position = new Vector3(LineStartPosition.x, wallHeight, LineStartPosition.y);
@@ -69,5 +73,113 @@ public class GenerateMesh : MonoBehaviour
             mr.material = mat;
 
         }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log(MeshToString(this.GetComponentInChildren<MeshFilter>(), Vector3.one));
+
+            using (StreamWriter streamWriter = new StreamWriter(string.Format("{0}{1}.obj", Application.dataPath+ "/_Obj/", this.gameObject.name)))
+            {
+                streamWriter.Write(MeshToString(this.GetComponentInChildren<MeshFilter>(), new Vector3(-1f, 1f, 1f)));
+                streamWriter.Close();
+            }
+            AssetDatabase.Refresh();
+        }
+
     }
+
+
+    /// <summary>
+    /// 轉換成obj
+    /// </summary>
+    /// <param name="mf"></param>
+    /// <param name="scale"></param>
+    /// <returns></returns>
+    private string MeshToString(MeshFilter mf, Vector3 scale)
+    {
+        Mesh mesh = mf.mesh;
+        Material[] sharedMaterials = mf.GetComponent<Renderer>().sharedMaterials;
+        Vector2 textureOffset = mf.GetComponent<Renderer>().material.GetTextureOffset("_MainTex");
+        Vector2 textureScale = mf.GetComponent<Renderer>().material.GetTextureScale("_MainTex");
+
+        StringBuilder stringBuilder = new StringBuilder().Append("mtllib design.mtl")
+            .Append("\n")
+            .Append("g ")
+            .Append(mf.name)
+            .Append("\n");
+
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vector = vertices[i];
+            stringBuilder.Append(string.Format("v {0} {1} {2}\n", vector.x * scale.x, vector.y * scale.y, vector.z * scale.z));
+        }
+
+        stringBuilder.Append("\n");
+
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+        if (mesh.subMeshCount > 1)
+        {
+            int[] triangles = mesh.GetTriangles(1);
+
+            for (int j = 0; j < triangles.Length; j += 3)
+            {
+                if (!dictionary.ContainsKey(triangles[j]))
+                {
+                    dictionary.Add(triangles[j], 1);
+                }
+
+                if (!dictionary.ContainsKey(triangles[j + 1]))
+                {
+                    dictionary.Add(triangles[j + 1], 1);
+                }
+
+                if (!dictionary.ContainsKey(triangles[j + 2]))
+                {
+                    dictionary.Add(triangles[j + 2], 1);
+                }
+            }
+        }
+
+        for (int num = 0; num != mesh.uv.Length; num++)
+        {
+            Vector2 vector2 = Vector2.Scale(mesh.uv[num], textureScale) + textureOffset;
+
+            if (dictionary.ContainsKey(num))
+            {
+                stringBuilder.Append(string.Format("vt {0} {1}\n", mesh.uv[num].x, mesh.uv[num].y));
+            }
+            else
+            {
+                stringBuilder.Append(string.Format("vt {0} {1}\n", vector2.x, vector2.y));
+            }
+        }
+
+        for (int k = 0; k < mesh.subMeshCount; k++)
+        {
+            stringBuilder.Append("\n");
+
+            if (k == 0)
+            {
+                stringBuilder.Append("usemtl ").Append("Material_design").Append("\n");
+            }
+
+            if (k == 1)
+            {
+                stringBuilder.Append("usemtl ").Append("Material_logo").Append("\n");
+            }
+
+            int[] triangles2 = mesh.GetTriangles(k);
+
+            for (int l = 0; l < triangles2.Length; l += 3)
+            {
+                stringBuilder.Append(string.Format("f {0}/{0} {1}/{1} {2}/{2}\n", triangles2[l] + 1, triangles2[l + 2] + 1, triangles2[l + 1] + 1));
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+
 }
